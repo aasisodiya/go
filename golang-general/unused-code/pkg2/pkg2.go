b2f5e3ed0f247638d6ae25598cd0c1dfb65830c4
Akashsingh Sisodiya <akashsinghsisodiya@gmail.com>
2023-09-13 01:25:05 +0530

    Documentation: Identify Unused Code in Code

---
+++ golang-general/unused-code/pkg2/pkg2.go
@@ -0,0 +1,17 @@
+package pkg2
+
+import (
+	"fmt"
+)
+
+func Pkg2Test() {
+	fmt.Println("Testing Pkg2Test")
+}
+
+func pkg2Test() {
+	fmt.Println("Testing pkg2Test")
+}
+
+func Pkg2Test2() {
+	fmt.Println("Testing Pkg2Test2")
+}
