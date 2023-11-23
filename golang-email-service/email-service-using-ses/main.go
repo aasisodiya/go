cd1bb42e65bd0de0bc56c580f1583337ebd04430
Akashsingh Sisodiya <12576614+aasisodiya@users.noreply.github.com>
2023-11-23 23:59:25 +0530

    Refactor the code (#74)

--- golang-email-service/email-service-using-ses/main.go
+++ golang-email-service/email-service-using-ses/main.go
@@ -2,8 +2,8 @@ package main
 
 // Step 2: Import Package
 import (
-	"main/aws"
 	"fmt"
+	"main/aws"
 )
 
 // Step 3: Call Functions
@@ -11,26 +11,26 @@ func main() {
 	email := "akash_sisodiya@test.com"
 	target := "test@test.com"
 	fmt.Println("Main")
-	test, err := aws.ListValidEmails("us-west-2")
+	test, err := aws.ListValidEmails(aws.DefaultRegion)
 	printIfError(err)
 	fmt.Println(test)
-	test, err = aws.ListAllEmails("us-west-2")
+	test, err = aws.ListAllEmails(aws.DefaultRegion)
 	printIfError(err)
 	fmt.Println(test)
-	test2, err := aws.VerifyEmailID(target, "us-west-2")
+	test2, err := aws.VerifyEmailID(target, aws.DefaultRegion)
 	printIfError(err)
 	fmt.Println(test2)
-	test3, err := aws.SendEmail(target, email, "Subject", "Text", "<h1>HTML</h1>", "us-west-2") //won't show "Text" in email body
+	_, err = aws.SendEmail(target, email, "Subject", "Text", "<h1>HTML</h1>", aws.DefaultRegion) //won't show "Text" in email body
 	printIfError(err)
-	test3, err = aws.SendEmail(target, email, "Subject", "", "<h1>HTML</h1>", "us-west-2") // Only HTML
+	_, err = aws.SendEmail(target, email, "Subject", "", "<h1>HTML</h1>", aws.DefaultRegion) // Only HTML
 	printIfError(err)
-	test3, err = aws.SendEmail(target, email, "Subject", "Text", "", "us-west-2") // Only Text
+	test3, err := aws.SendEmail(target, email, "Subject", "Text", "", aws.DefaultRegion) // Only Text
 	printIfError(err)
 	fmt.Println(test3)
-	test4, err := aws.RemoveEmail(target, "us-west-2")
+	test4, err := aws.RemoveEmail(target, aws.DefaultRegion)
 	printIfError(err)
 	fmt.Println(test4)
-	err = aws.SESStatistics("us-west-2")
+	err = aws.SESStatistics(aws.DefaultRegion)
 	printIfError(err)
 }
 
